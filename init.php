2d030810c448c7a9df4c7b109f53283f82794bf0
Hoshuyama <54694426+Hoshuyama@users.noreply.github.com>
2019-09-04 17:01:27 +0900

    Update init.php

--- init.php
+++ init.php
@@ -1,14 +1,12 @@
 <?php
-// セッションスタート
 session_start();
-// http://(またはhttps://)からホストまでを取得
+
 $site_url = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'] . '/';
 define('HOME_URL', $site_url . 'myphp/github-Hoshuyama-contact-form/');
-// 自動返信メールの管理者メールアドレス
+
 define('ADMIN_MAIL', '');
-// HTMlエスケープ
 function h($str) {
 	return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
 }
-// 共通ファイル読み込み
+
 require_once( __DIR__ . '/utility.php');
